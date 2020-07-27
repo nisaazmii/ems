2627c4e127e335b5072e6316292dd70a88069e11
unknown <sufiah.afira01@gmail.com>
2020-07-27 22:18:46 +0800

    Sufiah add new controller for the Question function inside controller folder

---
+++ controller/EventOrganizerController/queController.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once '../../model/EventOrganizerData/quesInfo.php';
+
+class queController {
+	
+	//add question
+
+   public function askquestion()
+    {
+       
+       $askquestion = new queInfo();
+       
+       $askquestion->email = $_POST['email'];
+                
+       $askquestion->question = $_POST['question'];
+       
+       
+       $askquestion->askquestion();
+       
+       $message="Your Question has been send";
+                    echo "<SCRIPT type='text/javascript'> 
+                            alert('$message');
+                            window.location.href=('../../view/EventOrganizerInterface/submitInterface.php');
+                        </SCRIPT>";
+       }
+      
+    
+
+
