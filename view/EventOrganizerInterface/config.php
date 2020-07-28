7676f5614a6b523532fe5f3cf92b3927d67a0839
naifAhmedB <n.a.basalib@gmail.com>
2020-07-28 21:24:16 +0800

    google login

---
+++ view/EventOrganizerInterface/config.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once '../../vendor/autoload.php';
+
+$google_client = new Google_Client();
+
+$google_client->setClientId('352542689533-ff8kqroo1s6hbtk02p11lofkp06kf8ot.apps.googleusercontent.com');
+
+$google_client->setClientSecret('IGypphyqaQxf7z6yohSOk8aE');
+
+$google_client->setRedirectUri('http://localhost/ems/view/CustomerInterface/HomeInterface.php');
+
+$google_client->addScope('email');
+
+$google_client->addScope('profile');
+
+session_start();
+
+
+?>
